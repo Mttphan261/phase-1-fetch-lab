63cdcc26c86d44f61b6365640ea612fd1efd5bab
Matt Phan <mttphan261@gmail.com>
2023-04-13 19:54:34 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  //create fetch request to GoT API
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json()) 
+
+  //convert response to JSON
   
+  //call renderBooks function to render JSON data
+  .then((json) => renderBooks(json))
+  // To pass the tests, don't forget to return your fetch!
+
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +25,42 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   //grabbing total numbers of pages
+//   .then((json) => { 
+//     //assigning var to equal 0 to add total pages to it
+//     let totalPages = 0;
+//     //for loop, iterating through json array using condition until all books have been iterated through
+//     for (let i = 0; i < json.length; i++) {
+//     //adding var equaling 0 to number of pages extracted from iteration
+//       totalPages += json[i].numberOfPages;
+//     }
+//     //console logging totalPages var outside of the function. If inside function, would return value for each book first.
+//     console.log(totalPages);
+
+//   })
